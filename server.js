8dfc4a571e2e4e2061fa6e0368c7bb6b0063a3fb
TitusKI <kidumazie@gmail.com>
2024-12-04 22:08:52 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.json({ message: "Get all contacts" });
+});
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
