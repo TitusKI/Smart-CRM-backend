f74ae60b0741a49b64f483266c8299e59a0bc10a
TitusKI <kidumazie@gmail.com>
2024-12-05 23:02:57 +0300

    Refactor server setup to include routing, middleware, and error handling

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const helmet = require("helmet");
 
-app.get("/api/contacts", (req, res) => {
-  res.json({ message: "Get all contacts" });
+const userRoutes = require("./routes/userRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const leadRoutes = require("./routes/leadRoutes");
+
+const errorHandler = require("./middleware/errorHandler");
+connectDb();
+
+const app = express();
+app.use(helmet());
+
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/contacts", contactRoutes);
+app.use("/api/v1/leads", leadRoutes);
+app.use(errorHandler);
+process.on("uncaughtException", (err) => {
+  console.log("uncaught Exception");
+  console.log(err.name, err.message);
+  process.exit(1);
 });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log("server listening on port " + port);
 });
